bcac2fb3adc938ac8c08067f362f01736404e9e4
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-08-07 01:23:43 +0100

    contest done

--- app/Models/ContestVotes.php
+++ app/Models/ContestVotes.php
@@ -1,65 +1,93 @@
-<?php 
+<?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-class ContestVotes extends Model 
+
+class ContestVotes extends Model
 {
-	
+
 
 	/**
-     * The table associated with the model.
-     *
-     * @var string
-     */
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
 	protected $table = 'contest_votes';
-	
+
 
 	/**
-     * The table primary key field
-     *
-     * @var string
-     */
+	 * The table primary key field
+	 *
+	 * @var string
+	 */
 	protected $primaryKey = 'id';
-	
+
 
 	/**
-     * Table fillable fields
-     *
-     * @var array
-     */
+	 * Table fillable fields
+	 *
+	 * @var array
+	 */
 	protected $fillable = [
-		'email','category_id','candidate_id','vote_number','amount','payment_status','ip_address'
+		'email', 'category_id', 'candidate_id', 'vote_number', 'amount', 'payment_status', 'ip_address'
 	];
 	public $timestamps = false;
-	
+
 
 	/**
-     * Set search query for the model
+	 * Set search query for the model
 	 * @param \Illuminate\Database\Eloquent\Builder $query
 	 * @param string $text
-     */
-	public static function search($query, $text){
-		//search table record 
+	 */
+	public static function search($query, $text)
+	{
+		//search table record
 		$search_condition = '(
-				contest_votes.id LIKE ?  OR 
-				contest_votes.email LIKE ?  OR 
-				contest_votes.ip_address LIKE ? 
+				contest_votes.id LIKE ?  OR
+				contest_votes.email LIKE ?  OR
+				contest_votes.ip_address LIKE ?
 		)';
 		$search_params = [
-			"%$text%","%$text%","%$text%"
+			"%$text%", "%$text%", "%$text%"
 		];
 		//setting search conditions
 		$query->whereRaw($search_condition, $search_params);
 	}
-	
+
 
 	/**
-     * return list page fields of the model.
-     * 
-     * @return array
-     */
-	public static function listFields(){
-		return [ 
+	 * return list page fields of the model.
+	 *
+	 * @return array
+	 */
+	public static function listFields()
+	{
+		return [
+			"contest_votes.id AS id",
+			"contest_votes.email AS email",
+			"contest_votes.category_id AS category_id",
+			"contest_categories.name AS contestcategories_name",
+			"contest_votes.candidate_id AS candidate_id",
+			"users.firstname AS users_firstname",
+			"contest_votes.vote_number AS vote_number",
+			"contest_votes.amount AS amount",
+			"contest_votes.payment_status AS payment_status",
+			"contest_votes.updated_at AS updated_at",
+			"contest_votes.ip_address AS ip_address"
+		];
+	}
+
+
+	/**
+	 * return exportList page fields of the model.
+	 *
+	 * @return array
+	 */
+	public static function exportListFields()
+	{
+		return [
 			"contest_votes.id AS id",
 			"contest_votes.email AS email",
 			"contest_votes.category_id AS category_id",
@@ -69,39 +97,19 @@ class ContestVotes extends Model
 			"contest_votes.amount AS amount",
 			"contest_votes.payment_status AS payment_status",
 			"contest_votes.updated_at AS updated_at",
-			"contest_votes.ip_address AS ip_address" 
+			"contest_votes.ip_address AS ip_address"
 		];
 	}
-	
+
 
 	/**
-     * return exportList page fields of the model.
-     * 
-     * @return array
-     */
-	public static function exportListFields(){
-		return [ 
-			"contest_votes.id AS id",
-			"contest_votes.email AS email",
-			"contest_votes.category_id AS category_id",
-			"contest_votes.candidate_id AS candidate_id",
-			"users.firstname AS users_firstname",
-			"contest_votes.vote_number AS vote_number",
-			"contest_votes.amount AS amount",
-			"contest_votes.payment_status AS payment_status",
-			"contest_votes.updated_at AS updated_at",
-			"contest_votes.ip_address AS ip_address" 
-		];
-	}
-	
-
-	/**
-     * return view page fields of the model.
-     * 
-     * @return array
-     */
-	public static function viewFields(){
-		return [ 
+	 * return view page fields of the model.
+	 *
+	 * @return array
+	 */
+	public static function viewFields()
+	{
+		return [
 			"id",
 			"email",
 			"category_id",
@@ -111,18 +119,19 @@ class ContestVotes extends Model
 			"payment_status",
 			"created_at",
 			"updated_at",
-			"ip_address" 
+			"ip_address"
 		];
 	}
-	
+
 
 	/**
-     * return exportView page fields of the model.
-     * 
-     * @return array
-     */
-	public static function exportViewFields(){
-		return [ 
+	 * return exportView page fields of the model.
+	 *
+	 * @return array
+	 */
+	public static function exportViewFields()
+	{
+		return [
 			"id",
 			"email",
 			"category_id",
@@ -132,18 +141,19 @@ class ContestVotes extends Model
 			"payment_status",
 			"created_at",
 			"updated_at",
-			"ip_address" 
+			"ip_address"
 		];
 	}
-	
+
 
 	/**
-     * return edit page fields of the model.
-     * 
-     * @return array
-     */
-	public static function editFields(){
-		return [ 
+	 * return edit page fields of the model.
+	 *
+	 * @return array
+	 */
+	public static function editFields()
+	{
+		return [
 			"id",
 			"email",
 			"category_id",
@@ -151,7 +161,7 @@ class ContestVotes extends Model
 			"vote_number",
 			"amount",
 			"payment_status",
-			"ip_address" 
+			"ip_address"
 		];
 	}
 }
