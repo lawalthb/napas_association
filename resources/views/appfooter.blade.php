738c5cabf4365d09b341d808113518246b67fc6b
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-08-11 10:26:38 +0100

    logo and statick page

--- resources/views/appfooter.blade.php
+++ resources/views/appfooter.blade.php
@@ -6,14 +6,14 @@
 			</div>
 			<div class="col">
 				<div class="footer-links text-right">
-					<a href="{{ url('info/about') }}">About us</a> | 
-					<a href="{{ url('info/faq') }}">Help and FAQ</a> |
-					<a href="{{ url('info/contact') }}">Contact us</a>  |
-					<a href="{{ url('info/privacypolicy') }}">Privacy Policy</a> |
-					<a href="{{ url('info/termsandconditions') }}">Terms and Conditions</a>
+					<a href="{{ url('/') }}">Home</a>
+					<!-- <a href="{{ url('info/faq') }}">Help and FAQ</a> | -->
+					<!-- <a href="{{ url('info/contact') }}">Contact us</a>  | -->
+					<!-- <a href="{{ url('info/privacypolicy') }}">Privacy Policy</a> |
+					<a href="{{ url('info/termsandconditions') }}">Terms and Conditions</a> -->
 				</div>
 			</div>
-			
+
 		</div>
 	</div>
 </footer>
