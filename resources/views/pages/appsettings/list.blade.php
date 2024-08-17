b209e669e588893224cd318ad4ea21016ee1318d
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-08-18 00:54:07 +0100

    app settings fix

--- resources/views/pages/appsettings/list.blade.php
+++ resources/views/pages/appsettings/list.blade.php
@@ -1,59 +1,59 @@
-<!-- 
+<!--
 expose component model to current view
 e.g $arrDataFromDb = $comp_model->fetchData(); //function name
 -->
 @inject('comp_model', 'App\Models\ComponentsData')
 <?php
-    //check if current user role is allowed access to the pages
-    $can_add = $user->canAccess("appsettings/add");
-    $can_edit = $user->canAccess("appsettings/edit");
-    $can_view = $user->canAccess("appsettings/view");
-    $can_delete = $user->canAccess("appsettings/delete");
-    $field_name = request()->segment(3);
-    $field_value = request()->segment(4);
-    $total_records = $records->total();
-    $limit = $records->perPage();
-    $record_count = count($records);
-    $pageTitle = "App Settings"; //set dynamic page title
+//check if current user role is allowed access to the pages
+$can_add = $user->canAccess("appsettings/add");
+$can_edit = $user->canAccess("appsettings/edit");
+$can_view = $user->canAccess("appsettings/view");
+$can_delete = $user->canAccess("appsettings/delete");
+$field_name = request()->segment(3);
+$field_value = request()->segment(4);
+$total_records = $records->total();
+$limit = $records->perPage();
+$record_count = count($records);
+$pageTitle = "App Settings"; //set dynamic page title
 ?>
 @extends($layout)
 @section('title', $pageTitle)
 @section('content')
 <section class="page" data-page-type="list" data-page-url="{{ url()->full() }}">
     <?php
-        if( $show_header == true ){
+    if ($show_header == true) {
     ?>
-    <div  class="bg-light p-3 mb-3" >
-        <div class="container-fluid">
-            <div class="row justify-content-between align-items-center gap-3">
-                <div class="col  " >
-                    <div class="">
-                        <div class="h5 font-weight-bold text-primary">App Settings</div>
-                    </div>
-                </div>
-                <div class="col-auto  " >
-                </div>
-                <div class="col-md-3  " >
-                    <!-- Page drop down search component -->
-                    <form  class="search" action="{{ url()->current() }}" method="get">
-                        <input type="hidden" name="page" value="1" />
-                        <div class="input-group">
-                            <input value="<?php echo get_value('search'); ?>" class="form-control page-search" type="text" name="search"  placeholder="Search" />
-                            <button class="btn btn-primary"><i class="material-icons">search</i></button>
+        <div class="bg-light p-3 mb-3">
+            <div class="container-fluid">
+                <div class="row justify-content-between align-items-center gap-3">
+                    <div class="col  ">
+                        <div class="">
+                            <div class="h5 font-weight-bold text-primary">App Settings</div>
                         </div>
-                    </form>
+                    </div>
+                    <div class="col-auto  ">
+                    </div>
+                    <div class="col-md-3  ">
+                        <!-- Page drop down search component -->
+                        <form class="search" action="{{ url()->current() }}" method="get">
+                            <input type="hidden" name="page" value="1" />
+                            <div class="input-group">
+                                <input value="<?php echo get_value('search'); ?>" class="form-control page-search" type="text" name="search" placeholder="Search" />
+                                <button class="btn btn-primary"><i class="material-icons">search</i></button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     <?php
-        }
+    }
     ?>
-    <div  class="" >
+    <div class="">
         <div class="container-fluid">
             <div class="row ">
-                <div class="col comp-grid " >
-                    <div  class=" page-content" >
+                <div class="col comp-grid ">
+                    <div class=" page-content">
                         <div id="appsettings-list-records">
                             <div id="page-main-content" class="table-responsive">
                                 <?php Html::page_bread_crumb("/appsettings/", $field_name, $field_value); ?>
@@ -64,129 +64,105 @@ e.g $arrDataFromDb = $comp_model->fetchData(); //function name
                                 <table class="table table-hover table-striped table-sm text-left">
                                     <thead class="table-header ">
                                         <tr>
-                                            <th class="td-name" > Name</th>
-                                            <th class="td-value" > Value</th>
-                                            <th class="td-active" > Active</th>
+                                            <th class="td-name"> Name</th>
+                                            <th class="td-value"> Value</th>
+                                            <th class="td-active"> Active</th>
                                             <th class="td-btn"></th>
                                         </tr>
                                     </thead>
                                     <?php
-                                        if($total_records){
+                                    if ($total_records) {
                                     ?>
-                                    <tbody class="page-data">
-                                        <!--record-->
-                                        <?php
+                                        <tbody class="page-data">
+                                            <!--record-->
+                                            <?php
                                             $counter = 0;
-                                            foreach($records as $data){
-                                            $rec_id = ($data['id'] ? urlencode($data['id']) : null);
-                                            $counter++;
-                                        ?>
-                                        <tr>
-                                            <!--PageComponentStart-->
-                                            <td class="td-name">
-                                                <?php echo  $data['name'] ; ?>
-                                            </td>
-                                            <td class="td-value">
-                                                <span <?php if($can_edit){ ?> data-source='<?php print_link('componentsdata/value_option_list'); ?>' 
-                                                data-value="<?php echo $data['value']; ?>" 
-                                                data-pk="<?php echo $data['id'] ?>" 
-                                                data-url="<?php print_link("appsettings/edit/" . urlencode($data['id'])); ?>" 
-                                                data-name="value" 
-                                                data-title="Enter Value" 
-                                                data-placement="left" 
-                                                data-toggle="click" 
-                                                data-type="text" 
-                                                data-mode="popover" 
-                                                data-showbuttons="left" 
-                                                class="is-editable" <?php } ?>>
-                                                <?php echo  $data['value'] ; ?>
-                                                </span>
-                                            </td>
-                                            <td class="td-active">
-                                                <span <?php if($can_edit){ ?> data-step="any" 
-                                                data-source='<?php print_link('componentsdata/value_option_list'); ?>' 
-                                                data-value="<?php echo $data['active']; ?>" 
-                                                data-pk="<?php echo $data['id'] ?>" 
-                                                data-url="<?php print_link("appsettings/edit/" . urlencode($data['id'])); ?>" 
-                                                data-name="active" 
-                                                data-title="Enter Active" 
-                                                data-placement="left" 
-                                                data-toggle="click" 
-                                                data-type="number" 
-                                                data-mode="popover" 
-                                                data-showbuttons="left" 
-                                                class="is-editable" <?php } ?>>
-                                                <?php echo  $data['active'] ; ?>
-                                                </span>
-                                            </td>
-                                            <!--PageComponentEnd-->
-                                            <td class="td-btn">
-                                                <?php if($can_edit){ ?>
-                                                <a class="btn btn-sm btn-success has-tooltip "    href="<?php print_link("appsettings/edit/$rec_id"); ?>" >
-                                                <i class="material-icons">edit</i> Edit
-                                            </a>
-                                            <?php } ?>
-                                        </td>
-                                    </tr>
-                                    <?php 
-                                        }
-                                    ?>
-                                    <!--endrecord-->
-                                </tbody>
-                                <tbody class="search-data"></tbody>
-                                <?php
-                                    }
-                                    else{
-                                ?>
-                                <tbody class="page-data">
-                                    <tr>
-                                        <td class="bg-light text-center text-muted animated bounce p-3" colspan="1000">
-                                            <i class="material-icons">block</i> No record found
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                <?php
-                                    }
-                                ?>
-                            </table>
-                        </div>
-                        <?php
-                            if($show_footer){
-                        ?>
-                        <div class=" mt-3">
-                            <div class="row align-items-center justify-content-between">    
-                                <div class="col-md-auto d-flex">    
-                                    <?php if($can_delete){ ?>
-                                    <button data-prompt-msg="Are you sure you want to delete these records?" data-display-style="modal" data-url="<?php print_link("appsettings/delete/{sel_ids}"); ?>" class="btn btn-sm btn-danger btn-delete-selected d-none">
-                                    <i class="material-icons">delete_sweep</i> Delete Selected
-                                    </button>
-                                    <?php } ?>
-                                </div>
-                                <div class="col">   
+                                            foreach ($records as $data) {
+                                                $rec_id = ($data['id'] ? urlencode($data['id']) : null);
+                                                $counter++;
+                                            ?>
+                                                <tr>
+                                                    <!--PageComponentStart-->
+                                                    <td class="td-name">
+                                                        <?php echo  $data['name']; ?>
+                                                    </td>
+                                                    <td class="td-value">
+                                                        <span>
+                                                            <?php echo  $data['value']; ?>
+                                                        </span>
+                                                    </td>
+                                                    <td class="td-active">
+                                                        <span>
+                                                            <?php echo  $data['active']; ?>
+                                                        </span>
+                                                    </td>
+                                                    <!--PageComponentEnd-->
+                                                    <td class="td-btn">
+                                                        <?php if ($can_edit) { ?>
+                                                            <a class="btn btn-sm btn-success has-tooltip " href="<?php print_link("appsettings/edit/$rec_id"); ?>">
+                                                                <i class="material-icons">edit</i> Edit
+                                                            </a>
+                                                        <?php } ?>
+                                                    </td>
+                                                </tr>
+                                            <?php
+                                            }
+                                            ?>
+                                            <!--endrecord-->
+                                        </tbody>
+                                        <tbody class="search-data"></tbody>
                                     <?php
-                                        if($show_pagination == true){
-                                        $pager = new Pagination($total_records, $record_count);
-                                        $pager->show_page_count = false;
-                                        $pager->show_record_count = true;
-                                        $pager->show_page_limit =false;
-                                        $pager->limit = $limit;
-                                        $pager->show_page_number_list = true;
-                                        $pager->pager_link_range=5;
-                                        $pager->render();
-                                        }
+                                    } else {
                                     ?>
-                                </div>
+                                        <tbody class="page-data">
+                                            <tr>
+                                                <td class="bg-light text-center text-muted animated bounce p-3" colspan="1000">
+                                                    <i class="material-icons">block</i> No record found
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    <?php
+                                    }
+                                    ?>
+                                </table>
                             </div>
-                        </div>
-                        <?php
+                            <?php
+                            if ($show_footer) {
+                            ?>
+                                <div class=" mt-3">
+                                    <div class="row align-items-center justify-content-between">
+                                        <div class="col-md-auto d-flex">
+                                            <?php if ($can_delete) { ?>
+                                                <button data-prompt-msg="Are you sure you want to delete these records?" data-display-style="modal" data-url="<?php print_link("appsettings/delete/{sel_ids}"); ?>" class="btn btn-sm btn-danger btn-delete-selected d-none">
+                                                    <i class="material-icons">delete_sweep</i> Delete Selected
+                                                </button>
+                                            <?php } ?>
+                                        </div>
+                                        <div class="col">
+                                            <?php
+                                            if ($show_pagination == true) {
+                                                $pager = new Pagination($total_records, $record_count);
+                                                $pager->show_page_count = false;
+                                                $pager->show_record_count = true;
+                                                $pager->show_page_limit = false;
+                                                $pager->limit = $limit;
+                                                $pager->show_page_number_list = true;
+                                                $pager->pager_link_range = 5;
+                                                $pager->render();
+                                            }
+                                            ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
                             }
-                        ?>
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </section>
 
 
