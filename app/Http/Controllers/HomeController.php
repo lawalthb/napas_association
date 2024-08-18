bc6dc4879a482513b5ffded5d64fcbd5e4f2ead6
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-08-18 14:20:36 +0100

    member welcome page done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\SupervisorUsers;
+use App\Models\WebAbouts;
+use App\Models\WebExcos;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -24,11 +26,16 @@ class HomeController extends Controller
 			return view("pages.home.election_vote_page");
 		} elseif ($user->hasRole('Member')) {
 
+
+			//to get president details
+			$president  = WebExcos::where('post', 'President')->first() ?? 'No president found';
+			//to get about details
+			$about  = WebAbouts::where('id', 1)->first() ?? 'No about found';
 			// allocated supervisor for memeber
 			$supervisor = SupervisorUsers::with('supervisors')->where('user_id', $user->id)->first() ?? 'No supervisor found';
 
 
-			return view("pages.home.user", compact('supervisor'));
+			return view("pages.home.user", compact('supervisor', 'president', 'about'));
 		} else {
 			return view("pages.home.index");
 		}
--- resources/views/pages/home/user.blade.php
+++ resources/views/pages/home/user.blade.php
@@ -27,10 +27,13 @@ $pageTitle = "Member Dasbaord"; // set dynamic page title
         <div class="row ">
             <div class="col-md-9 comp-grid ">
                 <div class="card card-1 border rounded page-content">
-
-                    Welcome to the National Association of Business Administration and Management Students (NABAMS)!<br />
-                    <P>NABAMS is established with the vision of fostering excellence in the field of business administration and management, NABAMS is a dynamic organization dedicated to empowering students pursuing these disciplines. At NABAMS, we believe in providing a platform for students to network, learn, and grow professionally. Through a wide range of initiatives, including seminars, workshops, competitions, and networking events, we aim to equip our members with the knowledge, skills, and connections necessary to excel in the ever-evolving world of business. Our association is driven by a passionate team of students, educators, and industry professionals who are committed to supporting the academic and professional development of our members. Whether you're a seasoned business student or just starting your journey in the field, NABAMS offers valuable resources, opportunities, and support to help you succeed.
-
+                    <?php
+                    Html::page_img($president['image'], '250px', '300px', "", 1);
+                    ?>
+                    <p>I am <b>{{$president->name}}</b></p>
+                    {{$about->text}}<br />
+                    <P>
+                        {!!$about->body!!}
                     </P>
                     Your Supervisor is {{ isset($supervisor->supervisors->name) ? $supervisor->supervisors->name : 'No supervisor name found' }}
                     <br />
